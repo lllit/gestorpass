e97ff5f18e3f033a90b4d0fc8cf44e9250b84937
Matias <lit.io30303@gmail.com>
2025-05-06 14:27:01 -0400

    paginas lista - Favoritos - Logins - Tarjeta

--- app/(routes)/favourites/page.tsx
+++ app/(routes)/favourites/page.tsx
@@ -1,43 +1,37 @@
-import { getServerSession } from "next-auth"
-import { db } from "@/lib/db"
-import { redirect } from "next/navigation"
+import { getServerSession } from "next-auth";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 import { DataTableItems } from "@/components/Shared/DataTableItems";
 
-
-export default async function FavouritesPage(){
+export default async function FavouritesPage() {
   const session = await getServerSession();
-  if(!session || !session.user?.email){
-    return redirect("/")
-  } 
+  if (!session || !session.user?.email) {
+    return redirect("/");
+  }
   const user = await db.user.findUnique({
     where: {
       email: session?.user?.email,
-
     },
     include: {
-      elements:{
-        where:{
-          isFavourite:true,
+      elements: {
+        where: {
+          isFavourite: true,
         },
-        orderBy:{
+        orderBy: {
           createdAt: "desc",
-        }
-      }
-    }
+        },
+      },
+    },
+  });
 
-  })
-
-  if(!user || !user.elements){
+  if (!user || !user.elements) {
     redirect("/");
-    
   }
-  
 
   return (
     <div>
-      <h1>Lista de favoritos</h1>
+      <h1 className="text-xl md:text-3xl font-medium">Lista de elementos "Favoritos"</h1>
       <DataTableItems elements={user.elements}></DataTableItems>
     </div>
-  )
+  );
 }
-
